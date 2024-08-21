89df709def24ba433fe85fa94be47a0e158b811e
evanshiber <shiberevan@gmail.com>
2024-08-20 20:14:58 -0500

    implementing gabe's bilinking cursor animation

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Page loaded successfully!");
-    // Add any JavaScript functionality here
+document.addEventListener("DOMContentLoaded", function() {
+    const texts = [
+        'IT Operations Support Specialist',
+        'Host',
+        'Sales Associate',
+        'Volunteer'
+    ];
+
+    let count = 0;
+    let index = 0;
+    let currentText = '';
+    let letter = '';
+
+    (function type() {
+        if (count === texts.length) {
+            count = 0;
+        }
+
+        currentText = texts[count];
+        letter = currentText.slice(0, ++index);
+
+        document.querySelector('.hero--section--title span').textContent = letter;
+        if (letter.length === currentText.length) {
+            count++;
+            index = 0;
+        }
+
+        setTimeout(type, 300);
+    }());
 });
