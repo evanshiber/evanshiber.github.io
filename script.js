d5551ab4cad506df4b84b6162de96264675b89e2
evanshiber <shiberevan@gmail.com>
2025-06-14 06:27:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js';
 
-    pdfjsLib.getDocument('Faculty Presentation (1).pdf').promise.then(function(pdfDoc_) {
+    pdfjsLib.getDocument('491_slideshow.pdf').promise.then(function(pdfDoc_) {
         pdfDoc = pdfDoc_;
         pageCountDisplay.textContent = pdfDoc.numPages;
         renderPage(pageNum);
